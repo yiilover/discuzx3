da96fe2b8c7f7996977bd2615c6ae917eee01a2e
unknown <Administrator@MICROSOF-284AB8.(none)>
2013-07-09 00:07:40 +0800

    init

---
+++ discuz/data/sysdata/cache_domain.php
@@ -0,0 +1,28 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: 5be46493777937de13b2a7c56c608942
+
+$domain = array (
+  'defaultindex' => 'forum.php',
+  'holddomain' => 'www|*blog*|*space*|*bbs*',
+  'list' => 
+  array (
+  ),
+  'app' => 
+  array (
+    'portal' => '',
+    'forum' => '',
+    'group' => '',
+    'home' => '',
+    'default' => '',
+  ),
+  'root' => 
+  array (
+    'home' => '',
+    'group' => '',
+    'forum' => '',
+    'topic' => '',
+    'channel' => '',
+  ),
+);
+?>
---
+++ discuz/uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '0okm)OKM');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'e0feefb276905dfea4771cae34f0b3f6');
+define('UC_FOUNDERSALT', 'q6H1Qe');
+define('UC_KEY', 'H6l1weMaGbhbbbTane03I8mcUd21H0TdcbsfU3fax5Z6QcebZ9sa9e83k4i7HfAb');
+define('UC_SITEID', '86y1Ze7agb3b6b1aFeu3D86cddn1Q0tdAbgfC3vaM5v6GcXbw9gaAeQ3e437MfXb');
+define('UC_MYKEY', 'P6N1sekajbXbxbTaweF3X8scndW1b0pdIbNfT3Sa25O6Kc5be9UageB3f4Y7mf3b');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
