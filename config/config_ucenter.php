494fadfaaeb213c356a1efc980fd96887a60252e
yiilover <yiilover@gmail.com>
2013-07-10 23:57:04 +0800

    add

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '0okm)OKM');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'd2a9f7p8m9K5Se68Xf1a9381hc09i0qdZ6k71af2e9meY5m1N5Wdy8z57fx4o7v5');
+define('UC_API', 'http://localhost:8123/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
